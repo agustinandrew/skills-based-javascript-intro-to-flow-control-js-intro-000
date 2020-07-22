4b887a9ee5ac84a3f1fec46c5cbb34f881a4bb61
pragmatic-founder-4933 <agustinandrew@gmail.com>
2020-07-22 22:38:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,8 +15,7 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   return (age >=13 && age <=19) ?
-  "You are a teenager" : "you are not a teenager"
-}
+  "You are a teenager" : "You are not a teenager"}
 
 function switchAge(age) {
   switch(age){
